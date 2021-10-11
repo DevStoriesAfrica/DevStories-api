e8821cdae5476d8a301b93b88cc0f21eb1e9c716
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2021-10-11 17:08:21 +0300

    feat: Added responses file to format responses as success or error

--- api/responses/responses.go
+++ api/responses/responses.go
@@ -1,1 +1,24 @@
 package responses
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func SUCCESS(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.WriteHeader(statusCode)
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		fmt.Fprintf(w, "Error formatting response: %v", err)
+	}
+}
+
+func ERROR(w http.ResponseWriter, statusCode int, err error) {
+	if err != nil {
+		SUCCESS(w, statusCode, map[string]string{"error": err.Error()})
+		return
+	}
+
+	SUCCESS(w, http.StatusBadRequest, nil)
+}
